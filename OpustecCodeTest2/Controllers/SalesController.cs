d9435c67ce6bc95d108998b18c60441a36bc653c
shohag66 <professional.cse66@gmail.com>
2022-09-06 22:35:26 +0600

    Three

---
+++ OpustecCodeTest2/Controllers/SalesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpustecCodeTest2.Data;
+using OpustecCodeTest2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpustecCodeTest2.Controllers
+{
+    public class SalesController : Controller
+    {
+        private ApplicationDbContext _db;
+        public SalesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult getProducts(int id)
+        {
+            List<Product> products = new List<Product>();
+            
+                products = _db.Products.Where(a => a.Id.Equals(id)).OrderBy(a => a.ProductName).ToList();
+
+            return Json(products, new Newtonsoft.Json.JsonSerializerSettings());
+        }
+    }
+}
